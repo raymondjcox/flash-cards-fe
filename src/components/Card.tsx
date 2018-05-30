6b0af46892ff8d1ac25f366a795b585395170560
Raymond Cox <raymond@helios.raymondjcox.com>
2018-05-30 13:24:31 -0400

    More styling updates

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,13 +21,14 @@ const StyledCardText = styled.div`
   align-self: center;
   margin-top: auto;
   margin-bottom: auto;
+  width: 100%;
   max-width: 100%;
   h1 {
     font-size: 2.0rem;
     font-weight: 400;
   }
   p {
-    text-align: center;
+    text-align: left;
   }
   pre {
     background-color: #25292E;
